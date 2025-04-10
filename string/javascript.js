965b62209b8c619c895cd81d64fe0f5a1de42034
Rodrigo <rodrigomareque8@gmail.com>
2025-04-10 16:45:59 -0300

    Criando seção String

---
+++ string/javascript.js
@@ -0,0 +1,7 @@
+// Construtora de Strings.
+
+// Toda string possui as propriedades e métodos do prototype de String
+
+const comida = 'Pizza';
+const liquido = new String('Água');
+const ano = new String(2018);
